52ee6246871a712073ed7b2ad27cdb99a1d0e2cd
Octopus Octopus <oct2pus@jade.moe>
2018-01-21 08:04:33 -0600

    updated >invite to the proper invite.

--- src/modules/commands/invite.rb
+++ src/modules/commands/invite.rb
@@ -4,7 +4,7 @@ module Bot
     module Invite
       extend Discordrb::Commands::CommandContainer
       command(:invite, description: "posts an invite to jadebot") do |event|
-        event << '<https://discordapp.com/oauth2/authorize?client_id=331204502277586945&scope=bot&permissions=3126>'
+        event << '<https://discordapp.com/oauth2/authorize?client_id=404624723595493378&scope=bot&permissions=268790838>'
       end
     end
   end
